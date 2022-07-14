c8e03085a3ca74ad3439db147c998d24ddaac0f7
Leebumju <dlqkawns@naver.com>
2022-07-14 17:00:57 +0900

    스크롤 페이징 처리 작업 진행 중

--- Flooming/GalleryTableViewController.swift
+++ Flooming/GalleryTableViewController.swift
@@ -7,32 +7,133 @@
 
 import UIKit
 
-class GalleryTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-    @IBOutlet weak var galleryTableView: UITableView!
+class GalleryTableViewController: UIViewController {
     
-    var galleryList = ["1","2","3","4","5","6","7","8"]
+    @IBOutlet weak var tableView: UITableView!
+        
+    var cellDatas: [CellData] = [] // 셀에 표시될 데이터 리스트
+      
+    var isPaging: Bool = false // 현재 페이징 중인지 체크하는 flag
+    var hasNextPage: Bool = false // 마지막 페이지 인지 체크 하는 flag
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return galleryList.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "galleryCell", for: indexPath) as! GalleryCell
-        cell.comment.text = galleryList[indexPath.row]
+    override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            
+            paging()
+        }
         
-        return cell
+        func paging() {
+            let index = cellDatas.count
+            
+            var datas: [CellData] = []
+            
+            for i in index..<(index + 20) {
+                let data = CellData(title: "Title\(i)")
+                datas.append(data)
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                self.cellDatas.append(contentsOf: datas) // 데이터는 계속해서 append 시켜줌 (페이징의 핵심!)
+                
+                self.hasNextPage = self.cellDatas.count > 300 ? false : true // 다음 페이지가 있는지 여부를 표시
+                self.isPaging = false // 페이징이 종료 되었음을 표시
+                
+                self.tableView.reloadData()
+            }
+        }
+}
+
+class MyCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    //@IBOutlet weak var dateLabel: UILabel!
+}
+
+class LoadingCell: UITableViewCell {
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    func start() {
+        activityIndicatorView.startAnimating()
     }
 }
 
-class GalleryCell: UITableViewCell {
-//    @IBOutlet weak var gallaryImage: UIImageView!
-    @IBOutlet weak var comment: UILabel!
-    @IBOutlet weak var downloadImage: UIImageView!
+struct CellData {
+    let title: String
+}
+
+
+extension GalleryTableViewController: UITableViewDelegate, UITableViewDataSource {
+    // Section을 2개 설정하는 이유는 페이징 로딩 시 로딩 셀을 표시해주기 위해서입니다.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return cellDatas.count
+        } else if section == 1 && isPaging && hasNextPage {
+            return 1
+        }
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath) as? MyCell else {
+                return UITableViewCell()
+            }
+            
+            let data = cellDatas[indexPath.row]
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = .current
+            dateFormatter.timeZone = .current
+            dateFormatter.dateFormat = "yyyy. MM. dd"
+            
+            cell.titleLabel.text = data.title
+            
+            return cell
+        } else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "LoadingCell", for: indexPath) as? LoadingCell else {
+                return UITableViewCell()
+            }
+            
+            cell.start()
+            
+            return cell
+        }
+    }
+}
+
+
+extension GalleryTableViewController {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.height
+        
+        // 스크롤이 테이블 뷰 Offset의 끝에 가게 되면 다음 페이지를 호출
+        if offsetY > (contentHeight - height) {
+            if isPaging == false && hasNextPage {
+                beginPaging()
+            }
+        }
+    }
+    
+    func beginPaging() {
+        isPaging = true // 현재 페이징이 진행 되는 것을 표시
+        // Section 1을 reload하여 로딩 셀을 보여줌 (페이징 진행 중인 것을 확인할 수 있도록)
+        DispatchQueue.main.async {
+            self.tableView.reloadSections(IndexSet(integer: 1), with: .none)
+        }
+        
+        // 페이징 메소드 호출
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.paging()
+        }
+    }
 }
