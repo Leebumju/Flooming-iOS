f524badbe36f56db208719eb9b75f9b58a1fb1c8
Leebumju <dlqkawns@naver.com>
2022-07-14 15:50:58 +0900

    갤러리 화면 구성 중

--- Flooming/GalleryTableViewController.swift
+++ Flooming/GalleryTableViewController.swift
@@ -7,23 +7,32 @@
 
 import UIKit
 
-class GalleryTableViewController: UIViewController {
+class GalleryTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var galleryTableView: UITableView!
+    
+    var galleryList = ["1","2","3","4","5","6","7","8"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return galleryList.count
     }
-    */
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "galleryCell", for: indexPath) as! GalleryCell
+        cell.comment.text = galleryList[indexPath.row]
+        
+        return cell
+    }
+}
+
+class GalleryCell: UITableViewCell {
+//    @IBOutlet weak var gallaryImage: UIImageView!
+    @IBOutlet weak var comment: UILabel!
+    @IBOutlet weak var downloadImage: UIImageView!
 }
