0f70ed826e6cd556cda885980cac49418451a3db
Leebumju <dlqkawns@naver.com>
2022-08-24 16:40:49 +0900

    alamofireseviece.swift 통신 함수 작업 진행 중

---
+++ Flooming/Services/AlamofireServices.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  Flooming
+//
+//  Created by 이범준 on 8/24/22.
+//
+
+import Foundation
+
+class AlamofireServices {
+    func uploadPhoto(url: URL) {
+        
+    }
+    
+    func getBasicFlowerImage(url: URL) {
+        
+    }
+    
+    func getPhoto(url: URL) {
+        
+    }
+    
+    func createPicture(url: URL) {
+        
+    }
+    
+    func getAllGallery(url: URL) {
+        
+    }
+    
+    
+}
