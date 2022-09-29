a4eb75ae5f92239c1379c50810d60af24d70e9ed
Leebumju <dlqkawns@naver.com>
2022-09-29 15:16:51 +0900

    3번째 리젝 사유 수정

--- Flooming/OnBoardingSecondViewController.swift
+++ Flooming/OnBoardingSecondViewController.swift
@@ -9,16 +9,31 @@ import UIKit
 
 class OnBoardingSecondViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        textLabel.lineBreakMode = .byWordWrapping
+        textLabel.numberOfLines = 0
+        let attributedString = NSMutableAttributedString(string: "1. 다른 사용자에게 불쾌감, 피해를 주는 콘텐츠를 게시하지 않는다는 것에 동의합니다. \n\n2. 위 사항을 준수하지 않을 시 별다른 통보 없이 해당 게시물이 삭제될 수 있습니다.")
 
-        // Do any additional setup after loading the view.
+        // *** Create instance of `NSMutableParagraphStyle`
+        let paragraphStyle = NSMutableParagraphStyle()
+
+        // *** set LineSpacing property in points ***
+        paragraphStyle.lineSpacing = 10 // Whatever line spacing you want in points
+
+        // *** Apply attribute to string ***
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, attributedString.length))
+
+        // *** Set Attributed String to your label ***
+        textLabel.attributedText = attributedString
     }
     
 
     @IBAction func goToMainButton(_ sender: Any) {
         presentMainVC()
     }
+    
     private func presentMainVC() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "NavigationController") as! UINavigationController
